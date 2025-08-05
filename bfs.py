72fa2352f2bacf024be11f3937dad4a4677a39ec
kiruthika569 <lkiruthika673@gmail.com>
2025-08-05 15:34:52 +0530

    BFS

--- bfs.py
+++ bfs.py
@@ -1,10 +1,25 @@
+from collections import deque
 def bfs(graph,start_node):
     visited=set()
     queue=deque()
     queue.append(start_node)
     visited.add(start_node)
     print("BFS Traversal Order:")
+    #Loop through the queue
     while queue:
         current_node=queue.popleft()
-        print(current_node,end=' ')
+        print(current_node,end=' ')#process current code
+#Visit all unvisited neighbors
+        for neighbor in graph.get(current_node,[]):
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+#Example usage:Define the graph as an adjacency List
+graph_input={
+    'A':['B','C'],
+    'B':['D','E'],
+    'C':['F','G'],
+} 
+bfs(graph_input,'A')               
+            
         
